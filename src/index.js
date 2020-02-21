761389194e5a06b9cf160ff6293ace131bd78970
Alexander Sakhaev <alsakhaev@gmail.com>
2020-02-21 13:04:28 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './fakeapi_inpage.js';
+import '@dapplets/dapplet-extension/build/background.js';
+import '@dapplets/dapplet-extension/build/inpage.js';
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+/* global __dirname, require, module*/
+
+const webpack = require('webpack');
+const path = require('path');
+const env = require('yargs').argv.env; // use --env with webpack 2
+const pkg = require('./package.json');
+
+let libraryName = 'dapplet-jslib';
+
+let outputFile, mode;
+
+if (env === 'build') {
+  mode = 'production';
+  outputFile = libraryName + '.min.js';
+} else {
+  mode = 'development';
+  outputFile = libraryName + '.js';
+}
+
+const config = {
+  mode: mode,
+  entry: __dirname + '/src/index.js',
+  //devtool: 'inline-source-map',
+  output: {
+    path: __dirname + '/lib',
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    globalObject: "typeof self !== 'undefined' ? self : this"
+  }
+};
+
+module.exports = config;
